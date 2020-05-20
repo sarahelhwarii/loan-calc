e0593ca3627fef1f2eed78310dd9f234c10db6cf
Sarah Elhwarii <56038677+sarahelhwarii@users.noreply.github.com>
2020-05-20 13:42:50 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const amount = document.querySelector("#amount");
+const interest = document.querySelector("#interest");
+const years = document.querySelector("#years");
+const calculateBtn = document.querySelector(".btn");
+const monthlyPayment = document.querySelector("#monthly-payment");
+const totalPayment = document.querySelector("#total-payment");
+const totalInterestt = document.querySelector("#total-interest");
